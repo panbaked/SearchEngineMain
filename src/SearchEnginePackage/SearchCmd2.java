f10b98e576c6dd3b891c03aa89bfe60ebd2a3a67
Rasmus <rasmus@taarnby.org>
2012-12-10 22:10:42 +0100

    Clean up and added comments

--- src/SearchEnginePackage/SearchCmd2.java
+++ src/SearchEnginePackage/SearchCmd2.java
@@ -1,12 +1,10 @@
 package SearchEnginePackage;
 
-/* SearchCmd.java
-   Written by Rune Hansen
-   Updated by Alexandre Buisse <abui@itu.dk>
-*/
-
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader; 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner; // It is only used for get the user input on which file to search
 
 class HTMLList2 {
     String str;
@@ -19,139 +17,127 @@ class HTMLList2 {
 }
 
 class Searcher2 {
+    public static long time; // Variable for timer
     
-    public static long time;
-
-    public static int exists (HTMLList2 l, String word) {
-        
-        long startTime = System.currentTimeMillis();
-       int foundWords = 0;
-        String lastURL = "";
-        while (l != null) 
-        {
-            if(l.str.charAt(0) == '*')
-            {
-                lastURL = l.str.substring(6); //remove *PAGE:
+    public static int exists (HTMLList2 l, String word) { 
+        long startTime = System.currentTimeMillis(); // Start timer
+       
+        int foundWords = 0;
+        String lastURL = ""; // Variable to hold lastest url 
+       
+        while (l != null) {
+            if(l.str.charAt(0) == '*') {
+                lastURL = l.str.substring(6); //remove *PAGE: and update url variable
             } 
             
-            if (l.str.equals (word)) 
-            {
+            if (l.str.equals (word)) {
             	System.out.println ("The word has been found in: " + lastURL);
             	foundWords++;
             }
             l = l.next;
         }
-        long endTime = System.currentTimeMillis();
-        time = endTime - startTime;
-        return foundWords;
+        
+        long endTime = System.currentTimeMillis(); // Stop timer
+        time = endTime - startTime; // Set timer variable
+        return foundWords; // Return words
     }
 
     public static HTMLList2 readHtmlList (String filename) throws IOException {
-        
-        long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis(); // Start timer
         String name;
         HTMLList2 start, current, tmp;
 
-        // Open the file given as argument
-        BufferedReader infile = new BufferedReader(new FileReader(filename));
+        BufferedReader infile = new BufferedReader(new FileReader(filename)); // Open the file given as argument
 
         name = infile.readLine(); //Read the first line
         start = new HTMLList2(name, null);
         current = start;
         name = infile.readLine(); // Read the next line
+        
         while (name != null) {    // Exit if there is none
             tmp = new HTMLList2(name,null);
             current.next = tmp;
             current = tmp;            // Update the linked list
             name = infile.readLine(); // Read the next line
         }
+        
         infile.close(); // Close the file        
-         long endTime = System.currentTimeMillis();
-         time = endTime - startTime;
-         return start;
+        long endTime = System.currentTimeMillis(); // Stop timer
+        time = endTime - startTime; // Set timer variable
+        return start;
     }
 }
 
 public class SearchCmd2 {
     
-     public static enum searchFile
-   {
-      SMALL("small.txt"),
-      MEDIUM("medium.txt"),
-      LARGE("large.txt");
-      private final String path;
+    public static enum searchFile {
+        SMALL("small.txt"),
+        MEDIUM("medium.txt"),
+        LARGE("large.txt");
+        private final String path;
 
-      searchFile(String file)
-      {
-         this.path = file;
-      }
+        searchFile(String file) {
+            this.path = file;
+        }
 
-      public String file()
-      {
-         return this.path;
-      }
-   }
+        public String file() {
+            return this.path;
+        }
+    }
 
 
     public static void main (String[] args) throws IOException {
+        String name;
         
-      String name;
-      
-      //prompt the user which file to use
-      
-      Scanner readIn = new Scanner(System.in);
-      String file = null;
+        Scanner readIn = new Scanner(System.in); //prompt the user which file to use
+        String file = null;
 
-      while (file == null)
-      {
-         try
-         {
-            System.out.println("Please type which file you use for searching e.g. : small, medium, large");
-            file = readIn.next().toUpperCase();
-            if (!file.equals(searchFile.SMALL.toString()) & !file.equals(searchFile.MEDIUM.toString()) & !file.equals(searchFile.LARGE.toString()))
-            {
-               throw new Exception("Input must be: " + searchFile.SMALL.toString() + " / " + searchFile.MEDIUM.toString() + " / " + searchFile.LARGE.toString());
+        while (file == null) {
+            try {
+                System.out.println("Please type which file you use for searching e.g. : small, medium, large");
+                System.out.print("Type: ");
+                file = readIn.next().toUpperCase();
+                if (!file.equals(searchFile.SMALL.toString()) & !file.equals(searchFile.MEDIUM.toString()) & !file.equals(searchFile.LARGE.toString())) {
+                    throw new Exception("Input must be: " + searchFile.SMALL.toString() + " / " + searchFile.MEDIUM.toString() + " / " + searchFile.LARGE.toString());
+                }
+            } catch (Exception ex) {
+                file = null;
+                System.out.println("Invalid input: " + ex.getMessage());
             }
-         }
-         catch (Exception ex)
-         {
-            file = null;
-            System.out.println("Invalid input: " + ex.getMessage());
-         }
-      }
+        }
 
-      searchFile fileFormat = null;
-      if (file.equals(searchFile.SMALL.toString())) {
+        searchFile fileFormat = null;
+        
+        if (file.equals(searchFile.SMALL.toString())) {
             fileFormat = searchFile.SMALL;
         }
-      if (file.equals(searchFile.MEDIUM.toString())) {
+        
+        if (file.equals(searchFile.MEDIUM.toString())) {
             fileFormat = searchFile.MEDIUM;
         }
-      if (file.equals(searchFile.LARGE.toString())) {
+        
+        if (file.equals(searchFile.LARGE.toString())) {
             fileFormat = searchFile.LARGE;
         }
-
-      // Read the file and create the linked list
-      HTMLList2 l = Searcher2.readHtmlList(fileFormat.file());
-      System.out.println("Datafile loaded in " + Searcher2.time / 1000F + " seconds.");
+ 
+        HTMLList2 l = Searcher2.readHtmlList(fileFormat.file()); // Read the file and create the linked list
+        System.out.println("Datafile loaded in " + Searcher2.time / 1000F + " seconds."); // Print time used to build the list
        
-
-        // Ask for a word to search
+        
         BufferedReader inuser =
-            new BufferedReader (new InputStreamReader (System.in));
+            new BufferedReader (new InputStreamReader (System.in)); // Ask for a word to search
 
         System.out.println ("Hit return to exit.");
         boolean quit = false;
+        
         while (!quit) {
             System.out.print ("Search for: ");
             name = inuser.readLine(); // Read a line from the terminal
             if (name == null || name.length() == 0) {
                 quit = true;
-            } 
-            else
-            {
+            } else {
             	int instancesFound = Searcher2.exists(l, name);
-            	System.out.println("There were "+instancesFound+" instances found.Search time:" + Searcher2.time + " miliseconds");
+            	System.out.println("There were " + instancesFound + " instances found.\nSearch time:" + Searcher2.time + " miliseconds");
             }
         }
     }
--- src/SearchEnginePackage/SearchCmd3.java
+++ src/SearchEnginePackage/SearchCmd3.java
@@ -1,13 +1,7 @@
 package SearchEnginePackage;
 
-/* SearchCmd.java
-   Written by Rune Hansen
-   Updated by Alexandre Buisse <abui@itu.dk>
-   Updated by Richard Peck 05/10/12
-*/
-
 import java.io.*;
-import java.util.Scanner;
+import java.util.Scanner; // It is only used for get the user input on which file to search
 
 class HTMLList {
     String str;
@@ -21,47 +15,44 @@ class HTMLList {
 }
 
 class URLList {
-	String url;
-	URLList next;
-	
-	URLList(String _url, URLList n)
-	{
-		url = _url;
-		next = n;
-	}
+    String url;
+    URLList next;
+
+    URLList(String _url, URLList n) {
+        url = _url;
+        next = n;
+    }
 }
-class Searcher3 {
+
+class Searcher3 { 
+    public static long time; // Variable for timer
     
-    public static long time;
-	
-    public static boolean exists (HTMLList l, String word) {
-        
-        long startTime = System.currentTimeMillis();
+    public static boolean exists (HTMLList l, String word) {    
+        long startTime = System.currentTimeMillis(); // Start timer
+
         while (l != null) {
             if (l.str.equals (word)) {
-                
-                long endTime = System.currentTimeMillis();
-                time = endTime - startTime;
+                long endTime = System.currentTimeMillis(); // Stop timer
+                time = endTime - startTime; // Update variable
                 return true;
             }
             l = l.next;
         }
-        
-        long endTime = System.currentTimeMillis();
-        time = endTime - startTime;
+        long endTime = System.currentTimeMillis(); // Stop timer
+        time = endTime - startTime; // Update variable
         return false;
     }
 
-    public static HTMLList readHtmlList (String filename) throws IOException {
-        
-        long startTime = System.currentTimeMillis();
+    public static HTMLList readHtmlList (String filename) throws IOException {    
+        long startTime = System.currentTimeMillis(); // Start timer
         String name;
         HTMLList start, current, tmp;
         String lastURL = "";
-        // Open the file given as argument
-        BufferedReader infile = new BufferedReader(new FileReader(filename));
+        
+        BufferedReader infile = 
+                new BufferedReader(new FileReader(filename)); // Open the file given as argument
 
-        name = infile.readLine(); //Read the first line
+        name = infile.readLine(); // Read the first line
         start = new HTMLList (name, null);
         current = start;
         name = infile.readLine(); // Read the next line
@@ -70,169 +61,146 @@ class Searcher3 {
             lastURL = getURL(name);
         }
         
-        while(name != null)
-        {
-        	 if(isPage(name)) {
+        while(name != null) {
+            if(isPage(name)) {
                 lastURL = getURL(name);
+            } else {
+        	if(exists(start, name)) { // Have we found this name before, if so we add the last url to this name
+                    HTMLList existingEntry = find(start, name);
+                    URLList duplicateURLEntry = find(existingEntry.urlList, lastURL); // Check for this url already exisiting with this name
+                    
+                    if(duplicateURLEntry == null) {
+                        URLList urlList = new URLList(lastURL, null);
+                        urlList.next = existingEntry.urlList;
+                        existingEntry.urlList = urlList; // Swap our pointer to the new starting element in the list
+                    }
+                } else { // Add a new entry with a new urllist
+                    tmp = new HTMLList(name, null);
+                    tmp.urlList = new URLList(lastURL, null);
+                    current.next = tmp;
+                    current = tmp;
+                }			 
             }
-        	 else
-        	 {
-        		 if(exists(start, name)) //Have we found this name before, if so we add the last url to this name
-        		 {
-        			 HTMLList existingEntry = find(start, name);
-        		
-        			 URLList duplicateURLEntry = find(existingEntry.urlList, lastURL); //check for this url already exisiting with this name
-        			 if(duplicateURLEntry == null)
-        			 {
-        				 URLList urlList = new URLList(lastURL, null);
-        				 urlList.next = existingEntry.urlList;
-            			 existingEntry.urlList = urlList; //swap our pointer to the new starting element in the list
-        			 }
-        		
-        		 }
-        		 else //Add a new entry with a new urllist
-        		 {
-        			 tmp = new HTMLList(name, null);
-        			 tmp.urlList = new URLList(lastURL, null);
-        			 current.next = tmp;
-        			 current = tmp;
-        		 }
-        			 
-        	 }
-        	
-        	name = infile.readLine();
+            name = infile.readLine();
         }
-       
-        infile.close(); // Close the file
-        
-        long endTime = System.currentTimeMillis();
-        time = endTime - startTime;
+      
+        infile.close(); // Close the file  
+        long endTime = System.currentTimeMillis(); // Stop timer
+        time = endTime - startTime; // Update variable
         return start;
     }
     
-    public static boolean isPage(String line)
-    {
+    public static boolean isPage(String line) { // Check if it is a page
     	if(line.length() < 6) {
             return false;
         }
     	
     	if(line.substring(0, 6).equals("*PAGE:")) {
             return true;
-        }
-    	else {
+        } else {
             return false;
         }
     }
     
-    public static String getURL(String pageLine)
-    {
+    public static String getURL(String pageLine) { // Get url
     	if(pageLine.length() < 6) {
             return "";
         }
     	return pageLine.substring(6);
     }
     
-    public static HTMLList find(HTMLList l, String name)
-    {
-    	 while (l != null) {
-             if (l.str.equals (name)) {
-                 return l;
-             }
-             l = l.next;
-         }
-    	 return null;
+    public static HTMLList find(HTMLList l, String name) { // Find a word
+        while (l != null) {
+            if (l.str.equals (name)) {
+                return l;
+            }
+            l = l.next;
+        }
+        return null;
     }
     
-    public static URLList find(URLList l, String url)
-    {
-	   	 while (l != null) {
-	         if (l.url.equals (url)) {
-	             return l;
-	         }
-	         l = l.next;
-	     }
-		 return null;
+    public static URLList find(URLList l, String url) { // Find a url
+        while (l != null) {
+            if (l.url.equals (url)) {
+                return l;
+            }
+            l = l.next;
+        }
+        return null;
     }
-    public static void printURLs(HTMLList l)
-	{
+    
+    public static void printURLs(HTMLList l) {
     	System.out.println("URLs linked to "+ l.str);
-    	
     	URLList urlList = l.urlList;
     	int i = 0;
-		while(urlList != null)
-		{
-			System.out.println(urlList.url);
-			urlList = urlList.next;
-			i++;
-		}
-		System.out.println("There were "+ i + " URLs attached.Search time:" + Searcher3.time + " milliseconds");		
-		
-	}
+        
+        while(urlList != null) {
+            System.out.println(urlList.url);
+            urlList = urlList.next;
+            i++;
+        }
+        System.out.println("There were "+ i + " URLs attached.Search time:" + Searcher3.time + " milliseconds");
+    }
 }
 
 public class SearchCmd3 {
+    public static enum searchFile {
+        SMALL("small.txt"),
+        MEDIUM("medium.txt"),
+        LARGE("large.txt");
+        private final String path;
 
-    public static enum searchFile
-   {
+        searchFile(String file) {
+            this.path = file;
+        }
 
-      SMALL("small.txt"),
-      MEDIUM("medium.txt"),
-      LARGE("large.txt");
-      private final String path;
-
-      searchFile(String file)
-      {
-         this.path = file;
-      }
-
-      public String file()
-      {
-         return this.path;
-      }
-   }
+        public String file() {
+            return this.path;
+        }
+    }
 
 
     public static void main (String[] args) throws IOException {
-        
-      String name;
-      
-      //prompt the user which file to use
-      
-      Scanner readIn = new Scanner(System.in);
-      String file = null;
 
-      while (file == null)
-      {
-         try
-         {
+        String name;
+
+        //prompt the user which file to use
+
+        Scanner readIn = new Scanner(System.in);
+        String file = null;
+
+        while (file == null)
+        {
+            try
+            {
             System.out.println("Please type which file you use for searching e.g. : small, medium, large");
             file = readIn.next().toUpperCase();
             if (!file.equals(SearchCmd2.searchFile.SMALL.toString()) & !file.equals(SearchCmd2.searchFile.MEDIUM.toString()) & !file.equals(SearchCmd2.searchFile.LARGE.toString()))
             {
-               throw new Exception("Input must be: " + SearchCmd2.searchFile.SMALL.toString() + " / " + SearchCmd2.searchFile.MEDIUM.toString() + " / " + SearchCmd2.searchFile.LARGE.toString());
+                throw new Exception("Input must be: " + SearchCmd2.searchFile.SMALL.toString() + " / " + SearchCmd2.searchFile.MEDIUM.toString() + " / " + SearchCmd2.searchFile.LARGE.toString());
             }
-         }
-         catch (Exception ex)
-         {
+            }
+            catch (Exception ex)
+            {
             file = null;
             System.out.println("Invalid input: " + ex.getMessage());
-         }
-      }
+            }
+        }
 
-      SearchCmd2.searchFile fileFormat = null;
-      if (file.equals(SearchCmd2.searchFile.SMALL.toString())) {
+        SearchCmd2.searchFile fileFormat = null;
+        if (file.equals(SearchCmd2.searchFile.SMALL.toString())) {
             fileFormat = SearchCmd2.searchFile.SMALL;
         }
-      if (file.equals(SearchCmd2.searchFile.MEDIUM.toString())) {
+        if (file.equals(SearchCmd2.searchFile.MEDIUM.toString())) {
             fileFormat = SearchCmd2.searchFile.MEDIUM;
         }
-      if (file.equals(SearchCmd2.searchFile.LARGE.toString())) {
+        if (file.equals(SearchCmd2.searchFile.LARGE.toString())) {
             fileFormat = SearchCmd2.searchFile.LARGE;
         }
 
         // Read the file and create the linked list
         HTMLList l = Searcher3.readHtmlList (fileFormat.file());
-         System.out.println("Datafile loaded in " + Searcher3.time / 1000F + " seconds.");
+            System.out.println("Datafile loaded in " + Searcher3.time / 1000F + " seconds.");
 
         // Ask for a word to search
         BufferedReader inuser =
@@ -244,7 +212,7 @@ public class SearchCmd3 {
         while (!quit) {
             System.out.print ("Search for: ");
             name = inuser.readLine(); // Read a line from the terminal
-            
+
             if (name == null || name.length() == 0) {
                 quit = true;
             } else if ((currentEntry = Searcher3.find (l, name)) != null) {
@@ -254,6 +222,6 @@ public class SearchCmd3 {
                 System.out.println ("The word \""+name+"\" has NOT been found.Search time:" + Searcher3.time + " milliseconds");
             }
         }
-        
+
     }
 }
